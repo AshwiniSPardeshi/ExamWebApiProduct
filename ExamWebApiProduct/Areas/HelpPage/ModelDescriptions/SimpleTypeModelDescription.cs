123e237bd6ba3af2fb637f919c543d8ef3e602bf
Ashwini <pardeshia1998@gmail.com>
2022-02-19 18:14:55 +0530

    Add project files.

---
+++ ExamWebApiProduct/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ExamWebApiProduct.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ExamWebApiProduct/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExamWebApiProduct.WebApiApplication" Language="C#" %>
